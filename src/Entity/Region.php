25c3f226b24b77d6f0957bfe8922e380a5a00abb
Al Zee <z@alz.ee>
2023-12-25 11:20:47 +0800

    entity region

---
+++ src/Entity/Region.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RegionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RegionRepository::class)]
+class Region
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $label = null;
+
+    #[ORM\Column]
+    private ?bool $onMenu = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function isOnMenu(): ?bool
+    {
+        return $this->onMenu;
+    }
+
+    public function setOnMenu(bool $onMenu): static
+    {
+        $this->onMenu = $onMenu;
+
+        return $this;
+    }
+}
