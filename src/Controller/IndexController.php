9254c78f425a8a3cdb6ff1ed50fddc670049e53a
Al Zee <z@alz.ee>
2024-01-03 08:56:29 +0800

    nth

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,15 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Service\Data;
 
 class IndexController extends AbstractController
 {
-    #[Route('/', name: 'app_index')]
-    public function index(Data $data): Response
+    private $data;
+    
+    public function __construct(Data $data)
     {
-        $arr = $data->get();
-        $arr['slides'] = $data->getNodeByTag('carousel', 6);
+        $this->data = $data;
+    }
+
+    #[Route('/', name: 'app_index')]
+    public function index(Request $request): Response
+    {
+        $locale = $request->getLocale();
+        dump($locale);
+        $arr = $this->data->get();
+        $arr['slides'] = $this->data->getNodeByTag('carousel', 6);
         return $this->render('index/index.html.twig', $arr);
     }
 }
