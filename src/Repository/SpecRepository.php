b1dabf55ac169f93d5d7b759083341f4d5121604
Al Zee <z@alz.ee>
2024-01-04 14:23:39 +0800

    entity Spec

---
+++ src/Repository/SpecRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Spec;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Spec>
+ *
+ * @method Spec|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Spec|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Spec[]    findAll()
+ * @method Spec[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SpecRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Spec::class);
+    }
+
+//    /**
+//     * @return Spec[] Returns an array of Spec objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('s')
+//            ->andWhere('s.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('s.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Spec
+//    {
+//        return $this->createQueryBuilder('s')
+//            ->andWhere('s.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
