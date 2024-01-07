10b926de96dc01560e752d6fdb5c621becb2edad
Al Zee <z@alz.ee>
2024-01-07 17:10:57 +0800

    nth

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -28,6 +28,11 @@ class Category
     {
         $this->nodes = new ArrayCollection();
     }
+    
+    public function __toString(): string
+    {
+        return $this->name;
+    }
 
     public function getId(): ?int
     {
