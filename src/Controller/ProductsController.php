2d6ed8d7eb1cb17317ef98f536e4a7d206ae584f
Al Zee <z@alz.ee>
2024-01-08 00:41:16 +0800

    nth

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -25,10 +25,26 @@ class ProductsController extends AbstractController
     {
         $locale = $request->getLocale();
         $request = $this->data->findNodeByRegionAndLocale('request', $locale);
+        $hero = $this->data->findNodeByRegion('products-hero', 1)[0];
+        $sliders1 = $this->data->findNodeByTag('products-slider-1', 3);
+        $sliders2 = 1; 
+        $sliders3 = 1; 
+        $sliders4 = 1; 
+        $sliders5 = 1; 
+        $sliders6 = 1; 
+        $spec = 1;
         $data = [
           'class' => 'page-products position-absolute',
           'page_title' => $this->translator->trans('Products'),
           'request' => $request,
+          'hero' => $hero,
+          'sliders1' => $sliders1,
+          'sliders2' => $sliders2,
+          'sliders3' => $sliders3,
+          'sliders4' => $sliders4,
+          'sliders5' => $sliders5,
+          'sliders6' => $sliders6,
+          'spec' => $spec,
         ];
         return $this->render('products/index.html.twig', $data);
     }
