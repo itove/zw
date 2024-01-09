051ac9c7f0b7409ae27182b58f33cd9fc32a296b
Al Zee <z@alz.ee>
2024-01-09 14:21:29 +0800

    conf/beian/wechat/miniprog in controller About

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -30,6 +30,10 @@ class AboutController extends AbstractController
         $spare = $this->data->findNodeByRegionAndLocale('spare_guarantee', $locale);
         $honors = $this->data->findNodeByRegion('honor');
         $news = $this->data->findNodeByRegion('news', 3);
+        $conf = $this->data->findConfByLocale($locale);
+        $beian = $this->data->findNodeByRegion('beian', 1)[0];
+        $wechat = $this->data->findNodeByRegion('footer-wechatqr', 1)[0];
+        $miniprog = $this->data->findNodeByRegion('footer-miniprogqr', 1)[0];
         $data = [
           'page_title' => $this->translator->trans('About Us'),
           'class' => 'page-about',
@@ -39,6 +43,10 @@ class AboutController extends AbstractController
           'spare' => $spare,
           'honors' => $honors,
           'news' => $news,
+          'conf' => $conf,
+          'beian' => $beian,
+          'wechat' => $wechat,
+          'miniprog' => $miniprog,
         ];
         return $this->render('about/index.html.twig', $data);
     }
