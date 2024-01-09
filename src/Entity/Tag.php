eb1c92fb3c88348625099330cf4e39216c42ac14
Al Zee <z@alz.ee>
2024-01-09 13:17:12 +0800

    UniqueEntity assert in entity Tag/Category

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -6,7 +6,11 @@ use App\Repository\TagRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
+#[UniqueEntity('name')]
+#[UniqueEntity('label')]
 #[ORM\Entity(repositoryClass: TagRepository::class)]
 class Tag
 {
