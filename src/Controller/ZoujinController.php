fcc0e94163800968ff333177b5b369351946df7c
Al Zee <z@alz.ee>
2024-05-08 16:55:27 +0800

    nth

--- src/Controller/ZoujinController.php
+++ src/Controller/ZoujinController.php
@@ -5,14 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Service\Data;
 
 class ZoujinController extends AbstractController
 {
-    #[Route('/zoujin', name: 'app_zoujin')]
-    public function index(): Response
+    private $data;
+    
+    public function __construct(Data $data)
     {
-        return $this->render('zoujin/index.html.twig', [
-            'controller_name' => 'ZoujinController',
-        ]);
+        $this->data = $data;
+    }
+    #[Route('/zoujin', name: 'app_zoujin')]
+    public function index(Request $request): Response
+    {
+        $locale = $request->getLocale();
+        $conf = $this->data->findConfByLocale($locale);
+        $data = [
+            'conf' => $conf,
+        ];
+        return $this->render('zoujin/index.html.twig', $data);
     }
 }
