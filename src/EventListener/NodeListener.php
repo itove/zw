aa12cdb95f1b490b12cbdfef23e052606e11e590
Al Zee <z@alz.ee>
2024-05-13 19:20:33 +0800

    nth

---
+++ src/EventListener/NodeListener.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * vim:ft=php et ts=4 sts=4
+ * @version
+ * @todo
+ */
+
+namespace App\EventListener;
+
+use Symfony\Component\PasswordHasher\Hasher\NodePasswordHasherInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Node;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\Events;
+
+// #[AsEntityListener(event: Events::prePersist, entity: Node::class)]
+// #[AsEntityListener(event: Events::postPersist, entity: Node::class)]
+class NodeListener extends AbstractController
+{
+    // public function prePersist(Node $node, LifecycleEventArgs $event): void
+    // {
+    //     $em = $event->getEntityManager();
+    //     foreach ($node->getRegions() as $region) {
+    //         foreach ($region->getShareWith() as $r) {
+    //             $node->addRegion($r);
+    //         }
+    //     }
+    // }
+}
