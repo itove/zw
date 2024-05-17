2f0796a580b539e976f77ba2d6ba7dad7318c8ea
Al Zee <z@alz.ee>
2024-05-17 11:36:39 +0800

    nth

--- public/js/services.js
+++ public/js/services.js
@@ -160,7 +160,7 @@ $('.food_item').click(function(){
 		url: "/api/nodes/" + id,
 		success: function(result) {
 			console.log(result);
-			$('.popupCxt').html(result.summary)
+			$('.popupCxt').html(result.content)
 			$('html,body').css({
 				'overflow': 'hidden'
 			})
@@ -182,7 +182,7 @@ $('.spfood_slide').click(function(){
 		url: "/api/nodes/" + id,
 		success: function(result) {
 			console.log(result);
-			$('.popupCxt').html(result.summary)
+			$('.popupCxt').html(result.content)
 			$('html,body').css({
 				'overflow': 'hidden'
 			})
