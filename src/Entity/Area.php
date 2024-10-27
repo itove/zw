7cc7c718722af4d784dba1477987ba9aa8748743
Al Zee <z@alz.ee>
2024-10-27 18:29:54 +0800

    nth

--- src/Entity/Area.php
+++ src/Entity/Area.php
@@ -32,6 +32,11 @@ class Area
         $this->nodes = new ArrayCollection();
     }
 
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
