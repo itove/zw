886a3fb2df7bf99af024f6993adad84895b2e275
Al Zee <z@alz.ee>
2024-11-09 19:34:03 +0800

    use extends NodeCrudController in genCruds

--- src/Controller/Admin/genCruds.sh
+++ src/Controller/Admin/genCruds.sh
@@ -8,8 +8,18 @@
 
 ############### Main Part ###############
 
+
 for i in {1..50}
 do
-    cp NodeCrudController.php _N$i.php
-    sed -i "/class NodeCrudController/s/NodeCrudController/_N$i/" _N$i.php
+    # cp NodeCrudController.php _N$i.php
+    # sed -i "/class NodeCrudController/s/NodeCrudController/_N$i/" _N$i.php
+
+    echo "<?php
+
+namespace App\Controller\Admin;
+
+class _N$i extends NodeCrudController
+{
+}" > _N$i.php
+
 done
